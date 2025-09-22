58e893999649eebab74510d48a65683d55fe6d62
Ghoulouis <maikhanh3241@gmail.com>
2025-09-22 14:12:46 +0700

    add script

--- scripts/governance/setDebt.ts
+++ scripts/governance/setDebt.ts
@@ -9,11 +9,33 @@ async function main() {
   const { get } = deployments;
   const privateKey = process.env.TEST_DEPLOYER!;
   const wallet = new ethers.Wallet(privateKey, ethers.provider);
-  let vault = Vault__factory.connect((await get("TestVault")).address);
+  let vault = Vault__factory.connect((await get("TestVault")).address, ethers.provider);
   await setRole(await vault.getAddress(), wallet.address, ROLES.ADD_STRATEGY_MANAGER, wallet);
   await addStrategy(await vault.getAddress(), addresses.base.offChainStrategy, wallet);
   await setRole(await vault.getAddress(), wallet.address, ROLES.DEBT_MANAGER, wallet);
-  console.log(" set max debt ...");
-  await setDebt(await vault.getAddress(), addresses.base.offChainStrategy, parseUnits("1", 6), wallet);
+
+  let data = [];
+
+  let dataVault = await vault.vaultData();
+  data.push({
+    name: "UnAllocation",
+    value: dataVault.totalIdle,
+  });
+
+  let dataWasabi = await vault.strategies(addresses.base.wasabiUSDCERC4626);
+  data.push({
+    name: "Wasabi",
+    value: dataWasabi.currentDebt,
+  });
+
+  let dataOffChain = await vault.strategies(addresses.base.offChainStrategy);
+
+  data.push({
+    name: "OffChain",
+    value: dataOffChain.currentDebt,
+  });
+
+  console.table(data);
+  //await setDebt(await vault.getAddress(), addresses.base.offChainStrategy, parseUnits("4", 6), wallet);
 }
 main();
